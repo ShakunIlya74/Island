b3cc9c415b245bddb59f36f0a35b039b9a9170c1
ShakunIlya74 <maksdin11@gmail.com>
2024-03-04 16:10:17 +0100

    fix stats

--- code/research/statistics_about_videos.py
+++ code/research/statistics_about_videos.py
@@ -15,20 +15,17 @@ def get_video_duration(file_path):
 
 
 if __name__ == '__main__':
-    video_file_path = "path_to_your_video_file.mp4"
-    duration_seconds = get_video_duration(video_file_path)
-    if duration_seconds is not None:
-        print(f"The duration of the video is {duration_seconds} seconds.")
-
     # get the list of all the video files in a directory
-    directory_path = "../../data/short_videos/all"
+    directory_path = "../data/short_videos/all"
     video_files = [f for f in os.listdir(directory_path) if f.endswith('.mp4')]
     list_of_durations = []
-    for video_file in video_files:
+    for i, video_file in enumerate(video_files):
+
         video_file_path = os.path.join(directory_path, video_file)
         duration_seconds = get_video_duration(video_file_path)
         if duration_seconds is not None:
             list_of_durations.append(duration_seconds)
+            print(f"Processed video {i + 1}/{len(video_files)}: {video_file} : {duration_seconds} seconds")
     # build a histogram of the durations
     print(f"List of durations: {list_of_durations}")
     print(f"Number of videos: {len(list_of_durations)}")
